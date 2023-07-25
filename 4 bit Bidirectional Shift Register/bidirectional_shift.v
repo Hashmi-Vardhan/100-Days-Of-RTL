ed15ccbb6a0dd04b399b39b700f2fe06ba092f74
KUNDRAPU HASHMI VARDHAN <133083707+Hashmi-Vardhan@users.noreply.github.com>
2023-07-25 23:38:26 +0530

    Add files via upload

---
+++ 4 bit Bidirectional Shift Register/bidirectional_shift.v
@@ -0,0 +1,14 @@
+module bidirectional_shift(input clk,d,M,clr,output reg [3:0]Q);
+
+always @(posedge clk)
+begin
+  if (clr)
+   Q <= 0;
+  else begin
+    if(M)
+	 Q <= {Q[2:0], d};
+	else
+	 Q <= {d, Q[3:1]};
+   end
+   end
+endmodule
